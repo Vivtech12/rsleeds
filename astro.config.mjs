98b73a6dd544a5ccf189929f25719aaf97ac5857
unknown <joeyk2k@gmail.com>
2025-06-11 22:29:47 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
+
+import tailwindcss from '@tailwindcss/vite'
+
+// https://astro.build/config
+export default defineConfig({
+	vite :{
+		plugins: [
+			tailwindcss(),
+		  ],
+	},
+	site: "https://rstradesleedsltd.com",
+	integrations: [icon(), sitemap()],
+});
---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "RS TRADES LEEDS LTD is a trusted name in the wholesale and retail distribution of consumer electronics.";
+export const SITE_DESCRIPTION =
+	"We offer a diverse range of high-quality electronic products to both businesses and individual customers across the UK and beyond. Our commitment lies in providing dependable service, competitive pricing, and cultivating strong partnerships with leading global brands.";
+export const companyInfo = {
+	name: "RS TRADES LEEDS LTD",
+	vatNumber: "16037129478642741",
+	phone: "07473 623048",
+	email: "info@rstradesleedsltd.com",
+};
